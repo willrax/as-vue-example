9f1cf5c18914f4f3aa35722aacc74eccb69d2ac8
Will Raxworthy <me@willrax.com>
2017-08-31 13:35:49 +0100

    initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <input v-model="message">
+
+    <button @click="sendAlert">
+      Alert
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello',
+
+    props: ['defaultMessage'],
+
+    data () {
+      return {
+        message: this.defaultMessage
+      }
+    },
+
+    methods: {
+      sendAlert () {
+        alert(this.message)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  button {
+    color: red;
+  }
+</style>
